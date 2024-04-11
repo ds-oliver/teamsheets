0f6e8c99862a3978ebdac0014e77f51289aae864
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2024-04-10 17:22:20 -0700

    Add expander for app explanation and display leagues in about section in teamsheets.py

--- teamsheets.py
+++ teamsheets.py
@@ -347,7 +347,7 @@ def main():
         # add expander here to explain the app
     with st.expander("**About this app**", expanded=True):
         leagues = sorted(fbref_lineups["league_display"].unique().tolist())
-        leagues_list = "\n".join([f"- {league}" for league in leagues])
+        leagues_list = [f"- {league}" for league in leagues]
         st.markdown(
             f"""
             The main function of this app is to analyze the team lineups and player positions in football matches.
