3bc39ee23cf919c9834f48f93fdaf9996b2cb380
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2024-04-10 17:30:03 -0700

    Refactor teamsheets.py to include an expander for app explanation and display the leagues in the about section

--- teamsheets.py
+++ teamsheets.py
@@ -348,9 +348,16 @@ def main():
     with st.expander("**About this app**", expanded=True):
         leagues = sorted(fbref_lineups["league_display"].unique().tolist())
         leagues_list = "\n".join([f'- {league}' for league in leagues])
-        leagues_list
         st.markdown(
-            f"The main function of this app is to analyze the team lineups and player positions in football matches. This app contains data from the following leagues:{leagues_list}\nThis app uses the Apriori algorithm to analyze team lineups and player positions in football matches. You can select a season, team, and competition to view detailed player analysis and team profiles."
+            """
+            The main function of this app is to analyze the team lineups and player positions in football matches.
+            This app contains data from the following leagues:
+            
+            {}
+            
+            This app uses the Apriori algorithm to analyze team lineups and player positions in football matches.
+            You can select a season, team, and competition to view detailed player analysis and team profiles.
+            """.format(leagues_list)
         )
 
     # Streamlit UI for season, team, and competition selection
