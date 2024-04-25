9081f37a0b6590c280675b78b275325afb762027
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2024-04-24 21:05:06 -0700

    Refactor teamsheets.py to include "gtd_status" column in injured player dataframe

--- teamsheets.py
+++ teamsheets.py
@@ -794,7 +794,7 @@ def main():
                     & (team_injury_report["status"].notnull())
                 ].reset_index(drop=True)
                 st.write(f"Players who were injured for {selected_team}:")
-                st.dataframe(injured_players[["player", "reason", "status", "started", "reserve"]])
+                st.dataframe(injured_players[["player", "reason", "status", "started", "reserve", "gtd_status"]])
 
             st.warning("Please select player(s) for for player-specific analysis.")
     # if key error print column names and log the error
