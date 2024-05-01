cca0f1d83b808509125f866c873d7757b09b8eaa
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2024-05-01 13:02:50 -0700

    Update TEAMSHEETS_CSV_FILEPATH in teamsheets.py to use the latest file name

--- teamsheets.py
+++ teamsheets.py
@@ -14,7 +14,7 @@ warnings.filterwarnings("ignore")
 # ignore  FutureWarning
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
-TEAMSHEETS_CSV_FILEPATH = "scraped_teamsheets/teamsheets_data_2024043022.csv"
+TEAMSHEETS_CSV_FILEPATH = "scraped_teamsheets/teamsheets_data_2024050111.csv"
 INJURY_REPORTS_CSV_FILEPATH = "scraped_missing_players/ws_missing_players_20240424_v2.csv"
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
